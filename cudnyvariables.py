0d47635528874f5c5db8b668a7511498dc3c822f
feverrro <50637065+feverrro@users.noreply.github.com>
2019-05-15 11:03:33 -0700

    update 1
    Checking work actualizations

--- cudnyvariables.py
+++ cudnyvariables.py
@@ -7,7 +7,9 @@ x = '400'
 y = '500'
 
 print(int(x) != int(y))
+print('___________________________________________________')
 
+#tables
 tab = [True, False, True, True]
 tab_index = 2
 print(tab[tab_index])
@@ -27,6 +29,7 @@ print(popped)
 print(tab)
 tab.reverse()
 print(tab)
+print('___________________________________________________')
 
 num_1 = 4
 num_2 = 5
@@ -35,3 +38,121 @@ nums_tab = [num_1, num_2, 3, 4, 6]
 print(nums_tab)
 nums_tab.sort()
 print(nums_tab)
+
+nums_tab.extend(tab)
+print(nums_tab)
+print('___________________________________________________')
+
+tab_words = ['One', 'Two', 'Three', 'Four', 'Five']
+tab_words_str = ' - '.join(tab_words)
+print(tab_words_str)
+new_list = tab_words_str.split(' - ')
+print(new_list)
+print('___________________________________________________')
+
+#mutable
+#Lists
+list_1 = ['One', 'Two', 'Three', 'Four', 'Five']
+list_2 = list_1
+print(list_1)
+print(list_2)
+
+list_1[0] = 'Art'
+print(list_1)
+print(list_2)
+
+#immutable
+#Tuples
+tuple_1 = ('One', 'Two', 'Three', 'Four', 'Five')
+tuple_2 = tuple_1
+print(tuple_1)
+print(tuple_2)
+#to nie zadziala
+#tuple_1[0] = 'Art'
+#print(tuple_1)
+#print(tuple_2)
+print('___________________________________________________')
+
+#Sets
+cs_nums = {'One', 'Two', 'Three', 'Four', 'Five'}
+cs_nums_2 = {'Six', 'Two', 'Seven', 'Eight', 'Five'}
+print(cs_nums)
+print('One' in cs_nums)
+print('Six' in cs_nums)
+print(cs_nums.intersection(cs_nums_2))
+print(cs_nums.difference(cs_nums_2))
+print(cs_nums.union(cs_nums_2))
+print('___________________________________________________')
+
+#empty lists/touples/sets
+empty_list = []
+empty_list = list()
+
+empty_tuple = ()
+empty_tuple = tuple()
+
+empty_set = {} # <- not right
+empty_set = set()
+print('___________________________________________________')
+
+#Dictioneries
+car = {'brand': 'Renault', 'BHP': '98', 'add_ons': ['electric windows', 'automatic gearbox']}
+print(car['brand'])
+print(car.get('add_ons'))
+print(car.get('enigine', 'not found!'))
+car['engine'] = 'R4 1800ccm'
+car['brand'] = 'Citroen'
+print(car)
+car.update({'brand': 'Audi', 'BHP': '110', 'v-max': '180km/h'})
+print(car)
+# del car['engine']
+# print(car)
+engine = car.pop('engine')
+print(engine)
+print(len(car))
+print(car.values())
+print(car.items())
+
+for key, value in car.items():
+    print(key, value)
+print('___________________________________________________')
+
+#conditionals and bools
+condition = 'amp'
+if condition  == 'amp':
+    print('trafiles')
+elif condition == 'bmp':
+    print('so close')
+else:
+    print('pudlo')
+
+# conditionals:
+# and
+# or
+# not
+
+user = 'admin'
+logged_in = False
+if user == 'admin' and logged_in:
+    print('admin page')
+elif user == 'admin' and not logged_in:
+    print('please log in')
+else:
+    print('failed')
+
+tab_a = [1, 2, 3]
+tab_b = [1, 2, 3]
+
+print(tab_a == tab_b, id(tab_a))
+print(tab_a is tab_b, id(tab_b))
+print('')
+tab_b = tab_a
+print(tab_a == tab_b, id(tab_a))
+print(tab_a is tab_b, id(tab_b))
+
+False Values:
+False
+None
+Zero of any numeric type
+Any empty sequence. For example: '', (), []
+Any empty mapping. F
--- variables.py
+++ variables.py
@@ -1,59 +1,183 @@
-
 # Operations on strings/Operacje na lancuchach znaków
 
-message = 'Hello Flat'+ ' ' + 'World'
-new_message = message.replace('Flat', 'Spheric')
-print(new_message)
+# message = 'Hello Flat'+ ' ' + 'World'
+# new_message = message.replace('Flat', 'Spheric')
+# print(new_message)
 
 
-greeting = 'Witaj'
-name = 'uzytkowniku'
+# greeting = 'Witaj'
+# name = 'uzytkowniku'
 
-placeholder = f'{greeting.lower()} { name.upper()[0:]}. Koniec'
-print(placeholder)
+# placeholder = f'{greeting.lower()} { name.upper()[0:]}. Koniec'
+# print(placeholder)
 
-Sentence = 'sItanbuls assd'
+# Sentence = 'sItanbuls assd'
 
-print(Sentence[:7])
-print(Sentence.find('s'))
-
-
-
-changer = 'Hello World d lod'
-
-new_changer = changer.replace('lod', ' ,Universe is Flat')
+# print(Sentence[:7])
+# print(Sentence.find('s'))
 
 
-print(new_changer)
 
-print('g' + 3**3*'o' + 'l')
+# changer = 'Hello World d lod'
 
-# Liczby/Numbers
+# new_changer = changer.replace('lod', ' ,Universe is Flat')
 
 
-absolute = abs(-4)
+# print(new_changer)
 
-print('Wartosc bezgledna: ', absolute)
+# print('g' + 3**3*'o' + 'l')
 
-print('Reszta z dzielenia:')
+# # Liczby/Numbers
 
-print(3%2)
-print(2%2)
-print(4%2)
-print(5%2)
 
-print('Zaokraglanie liczb:')
+# absolute = abs(-4)
 
-print(round(7.4324,0))
+# # print('Wartosc bezgledna: ', absolute)
 
-print(round(5.7548, 3))
+# # print('Reszta z dzielenia:')
 
-num = 2
-num = 10
+#  print(3%2)
+#  print(2%2)
+#  print(4%2)
+#  print(5%2)
 
-print(num)
+# print('Zaokraglanie liczb:')
 
-x = int('100')
-y = int('a')
+# print(round(7.4324,0))
 
-print(x + y)
+# print(round(5.7548, 3))
+
+# num = 2
+# num = 10
+
+# print(num)
+
+# # x = int('100')
+# y = int('a')
+
+# print(x + y)
+
+# Lists/listy
+
+squares = [1, 4, 9, 16, 25]
+
+print(squares)
+
+squares_2 = squares + [36, 49, 64, 81, 100]
+
+print(squares_2)
+
+cubes = [1, 8, 27, 65, 125]
+
+cubes[3] = 64
+cubes[4] = 128
+
+print(cubes)
+
+cubes.append(256)
+
+print(cubes)
+print('___________________________________________________')
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
+print(letters)
+
+letters[:4] = ['A', 'B', 'C', 'D']
+print(letters)
+
+letters[4:] = []
+print(letters)
+
+letters[:] = []
+letters[0:2] = ['Nowa', 'lista']
+print(letters)
+
+len(letters) 
+print(len(letters))
+
+a = [1, 2, 3]
+n = ['Slownik', 'Ksiazka Fantasy', 'Poradnik']
+x = [a,n]
+print(x)
+print(x[1][1])
+
+x.insert(0, 'Pierwszy element')
+print(x)
+
+y = ['ostatni element', 'kolejna rzecz']
+x.extend(y)
+x.append(y)
+
+x.remove('ostatni element')
+print(x)
+
+popped = x.pop()
+print(popped)
+print(x)
+
+x.reverse()
+print(x)
+
+nums = [10, 20, 30, 40, 35]
+nums.sort(reverse=True)
+print(nums)
+
+print('___________________________________________________')
+
+courses = ['Math','English','Programming']
+print(courses)
+courses_sorted = sorted(courses)
+print(courses_sorted)
+
+print(courses.index('Math'))
+
+print('Art' in courses)
+print('Math' in courses)
+
+for course in courses:
+    print(course)
+
+print('___________________________________________________')
+
+for index, course in enumerate(courses, start=1):
+    print(index, course)
+print('___________________________________________________')
+
+course_str = ' -'.join(courses) + '!'
+
+print(course_str)
+
+new_list = course_str.split(' -')
+
+print(course_str)
+print(new_list)
+
+print('___________________________________________________')
+
+# Tuples
+
+tuple_1 = ('History', 'Math', 'Physics','CompSci')
+tuple_2 = tuple_1
+
+print(tuple_1)
+print(tuple_2)
+
+print('___________________________________________________')
+
+cs_courses  = {'Art', 'Math', 'Physics','CompSci'}
+art_courses  = {'Art', 'Math', 'Desing','Modeling'}
+
+print('Math' in cs_courses)
+print(cs_courses.intersection(art_courses))
+print(cs_courses.difference(art_courses))
+print(cs_courses.union(art_courses))
+
+print('___________________________________________________')
+
+empty_list = []
+empty_list = list()
+
+empty_tuple = ()
+empty_tuple = tuple()
+
+empty_set = set()
