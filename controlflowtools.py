84f54fcf3749197058cb4e1669358d32319d6072
feverrro <50637065+feverrro@users.noreply.github.com>
2019-05-15 14:04:27 -0700

    Update controlflowtools.py

--- controlflowtools.py
+++ controlflowtools.py
@@ -1,8 +1,16 @@
 
-condition = None
+condition = ''
 
 if condition:
     print('Evaluating to True')
 else:
     print('Evaluating to False')
-    
+
+import sys
+
+print(sys.version)
+
+print('------------------------------------------------------------------------------------')
+
+print(sys.executable)
+
